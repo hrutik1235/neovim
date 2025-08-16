98c03666f744ee4b3332c33289f6235589d9c77e
hrutik03 <56082510+hrutik5321@users.noreply.github.com>
2025-08-16 12:27:05 +0530

    initial neovim setup added

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,15 @@
+return {
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile"
+  },
+  
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open Lazygit" },
+  },
+}
